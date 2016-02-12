85504f4d3769054df096026445f7c0e22108183d
Robert Collins <beaucollins@gmail.com>
2016-02-12 12:34:34 -0800

    Organizing and adding some comments
    - Removing unused function
    - Moving like functions next to each other

--- client/components/live-chat/index.jsx
+++ client/components/live-chat/index.jsx
@@ -10,15 +10,31 @@ import { openChat, closeChat, minimizeChat, updateChatMessage, sendChatMessage, 
 
 const debug = require( 'debug' )( 'calypso:live-chat:component' )
 const logger = log( debug )
-const chatIsAvailable = propExists( 'available' )
 
 const dispatchOpenChat = actionDispatcher( openChat )
 const dispatchCloseChat = actionDispatcher( closeChat )()
 const dispatchMinimizeChat = actionDispatcher( minimizeChat )()
 const dispatchUpdateChatMessageOnChange = actionDispatcher( updateChatMessage )( ( event ) => event.target.value )
+const dispatchSendMessage = actionDispatcher( sendChatMessage )
 
+const returnPressed = ( e ) => e.which === 13
+const preventDefault = ( e ) => e.preventDefault()
+
+const isAvailable = propExists( 'available' )
+const isConnecting = propEquals( 'connectionStatus', 'connecting' )
+const isConnected = propEquals( 'connectionStatus', 'connected' )
+const isChatOpen = all(
+	isAvailable,
+	any( isConnected, isConnecting )
+)
+const timelineHasContent = ( { timeline } ) => isArray( timeline ) && !isEmpty( timeline )
+
+/*
+ * Renders chat title message to display when chat is not active.
+ * Allows for different messages depending on whether live chats are available.
+ */
 const availabilityTitle = when(
-	chatIsAvailable,
+	isAvailable,
 	( { dispatch, user } ) => <div onClick={ dispatchOpenChat( pure( user ) )( dispatch ) }>Help</div>,
 	() => <div>Live Support Unavailable</div>
 )
@@ -32,24 +48,15 @@ const connectedTitle = ( { dispatch } ) => (
 	</div>
 )
 
-const isConnecting = propEquals( 'connectionStatus', 'connecting' )
-const isConnected = propEquals( 'connectionStatus', 'connected' )
-
 const title = first(
 	when( isConnected, connectedTitle ),
 	when( isConnecting, connectingTitle ),
 	availabilityTitle
 )
 
-const isChatOpen = all(
-	chatIsAvailable,
-	any( isConnected, isConnecting )
-)
-
-const dispatchSendMessage = actionDispatcher( sendChatMessage )
-const returnPressed = ( e ) => e.which === 13
-const preventDefault = ( e ) => e.preventDefault()
-
+/*
+ * Renders a textarea to be used to comopose a message for the chat.
+ */
 const renderComposer = ( { dispatch, message } ) => (
 	<div className="live-chat-composer">
 		<div className="live-chat-message">
@@ -67,24 +74,27 @@ const renderComposer = ( { dispatch, message } ) => (
 	</div>
 )
 
+/*
+ * Renders a single line of message text prefixed by the provided `nick`.
+ */
 const messageTextWithNick = ( { message, nick, key } ) => <p key={ key }><span className="message-nick">{ nick }</span> { message }</p>
+
+/*
+ * Renders a single line of message text.
+ */
 const messageText = ( { message, key } ) => <p key={ key }>{ message }</p>
 
 const messageAvatar = when( propExists( 'meta.image' ), ( { meta } ) => <img alt={ meta.nick } src={ meta.image } /> )
 
+/*
+ * Picks which message render function to call based on whether the message is from the currently logged in
+ * user.
+ */
 const renderMessage = when( propExists( 'isCurrentUser' ), messageText, messageTextWithNick )
-const renderTimelineItem = ( { item, isCurrentUser } ) => {
-	let [ message, meta ] = item
-	return (
-		<div className={ classnames( 'live-chat-timeline-message', { userMessage: isCurrentUser } ) } key={ meta.id }>
-			<div className="message-text">{ renderMessage( { isCurrentUser, message, nick: meta.nick } ) }</div>
-			<div className="message-meta">
-				<div className="message-avatar">{ messageAvatar( { meta } ) }</div>
-			</div>
-		</div>
-	)
-}
 
+/*
+ * Renders a chat bubble with multiple messages grouped by user.
+ */
 const renderGroupedTimelineItem = ( { item, isCurrentUser } ) => {
 	let [ initial, ... rest ] = item
 	let [ message, meta ] = initial
@@ -101,6 +111,9 @@ const renderGroupedTimelineItem = ( { item, isCurrentUser } ) => {
 	)
 }
 
+/*
+ * Renders a spinner in a flex-box context so it is centered vertically and horizontally
+ */
 const renderLoading = () => (
 	<div className="live-chat-loading">
 		<Spinner />
@@ -120,9 +133,13 @@ const groupMessages = ( messages ) => {
 	return grouped.groups.concat( [ grouped.group ] )
 }
 
+/*
+ * Returns a function for a component's ref property to enable autoscroll detection
+ */
 const autoScroll = ( { dispatch, autoscroll } ) => ( ref ) => {
 	if ( !ref ) return
 
+	// Scroll to the bottom of the chat transcript if autoscroll is enabled
 	if ( autoscroll ) ref.scrollTop = Math.max( 0, ref.scrollHeight - ref.offsetHeight )
 
 	ref.addEventListener( 'scroll', () => {
@@ -130,16 +147,15 @@ const autoScroll = ( { dispatch, autoscroll } ) => ( ref ) => {
 	} )
 }
 
-// TODO: auto-scroll
 const renderConversation = ( { timeline, isCurrentUser, autoscroll, dispatch } ) => (
 	<div ref={ autoScroll( { dispatch, autoscroll } ) } className="live-chat-conversation">
 		{ groupMessages( timeline ).map( ( item ) => renderGroupedTimelineItem( { item, isCurrentUser: isCurrentUser( item[0] ) } ) ) }
 	</div>
 )
 
-const timelineHasContent = ( { timeline } ) => isArray( timeline ) && !isEmpty( timeline )
-
-const welcomeMessage = () => <div className="live-chat-welcome">This is the beginning of your chat history with WordPress.com support. A chat history will be stored here.</div>
+const welcomeMessage = () => (
+	<div className="live-chat-welcome">This is the beginning of your chat history with WordPress.com support. A chat history will be stored here.</div>
+)
 
 const renderTimeline = first(
 	when( isConnecting, renderLoading ),
