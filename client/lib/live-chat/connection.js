3b3ecbccf9fb065c367b44d7c5d31d980629690b
Robert Collins <beaucollins@gmail.com>
2016-05-16 15:38:24 -0700

    Semicolons and style issues

--- client/lib/live-chat/connection.js
+++ client/lib/live-chat/connection.js
@@ -1,45 +1,39 @@
-const debug = require( 'debug' )( 'calypso:live-chat:connection' )
+import IO from 'socket.io-client';
+import { EventEmitter } from 'events';
+import { pick, mapKeys, get, assign } from 'lodash/object';
+import config from 'config';
+import { v4 as uuid } from 'uuid';
 
-import IO from 'socket.io-client'
-import { EventEmitter } from 'events'
-import { pick, mapKeys, get } from 'lodash/object'
-import config from 'config'
-import { v4 as uuid } from 'uuid'
+const debug = require( 'debug' )( 'calypso:live-chat:connection' );
 
 const USER_KEY_MAP = {
 	avatar_URL: 'picture',
 	ID: 'id',
 	display_name: 'name'
-}
+};
 
-const mapWPComUserKeys = ( value, key ) => get( USER_KEY_MAP, key, key )
-
-const emitActionMessage = ( connection ) => ( { id, type, user, message, timestamp, links } ) => {
-	debug( 'received socket action', { id, type, links } )
-	connection.emit( 'event', { id, type, links, user: {
-		nick: user.name,
-		picture: user.picture,
-		id: user.id,
-	}, timestamp, message } )
-}
+const mapWPComUserKeys = ( value, key ) => get( USER_KEY_MAP, key, key );
 
 const emitMessage = ( connection ) => ( { id, text, timestamp, user } ) => {
-	debug( 'received message', text )
+	debug( 'received message', text );
 	connection.emit( 'event', { id, type: 'message', timestamp, message: text, user: {
 		nick: user.displayName,
 		picture: user.avatarURL,
 		id: user.id
-	} } )
-}
+	} } );
+};
 
-const p = ( ... args ) => new Promise( ... args )
+
+const p = ( ... args ) => new Promise( ... args );
 
 class Connection extends EventEmitter {
 
 	connect( user ) {
-		if ( this.socket ) return p( ( resolve ) => resolve() )
+		if ( this.socket ) {
+			return p( ( resolve ) => resolve() );
+		}
 		return p( ( resolve ) => {
-			const socket = this.socket = new IO( config( 'live_chat_server_url' ) )
+			const socket = this.socket = new IO( config( 'live_chat_server_url' ) );
 			socket
 				.once( 'connect', () => resolve( socket ) )
 				.on( 'init', ( ... args ) => debug( 'initialized', ... args ) )
@@ -47,41 +41,41 @@ class Connection extends EventEmitter {
 					mapKeys( pick( user, [ 'ID', 'avatar_URL', 'display_name', 'username' ] ), mapWPComUserKeys )
 				) )
 				.on( 'token', ( handler ) => handler( user ) )
-				.on( 'message', emitMessage( this ) )
-		} )
+				.on( 'message', emitMessage( this ) );
+		} );
 	}
 
 	getSocket() {
 		return p( ( resolve, reject ) => {
-			if ( !this.socket ) return reject( new Error( 'not connected' ) )
-			resolve( this.socket )
-		} )
+			if ( !this.socket ) return reject( new Error( 'not connected' ) );
+			resolve( this.socket );
+		} );
 	}
 
 	open( user ) {
-		debug( 'open connection for user', user, config( 'live_chat_server_url' ) )
+		debug( 'open connection for user', user, config( 'live_chat_server_url' ) );
 
 		return this.connect( user ).then( ( socket ) => p( ( resolve ) => {
-			debug( 'connected', socket )
-			resolve()
-		} ) )
+			debug( 'connected', socket );
+			resolve();
+		} ) );
 	}
 
 	typing( message ) {
 		this.getSocket()
 		.then( ( socket ) => socket.emit( 'typing', { message } ) )
-		.catch( debug )
+		.catch( debug );
 	}
 
 	send( message ) {
 		this.getSocket()
 		.then( ( socket ) => new Promise( ( resolve ) => {
-			const id = uuid()
-			socket.emit( 'action', { message, type: 'message', id }, resolve )
-			socket.emit( 'message', { text: message, id }, resolve )
-		} ) )
+			const id = uuid();
+			socket.emit( 'action', { message, type: 'message', id }, resolve );
+			socket.emit( 'message', { text: message, id }, resolve );
+		} ) );
 	}
 
 }
 
-export default () => new Connection()
+export default () => new Connection();
