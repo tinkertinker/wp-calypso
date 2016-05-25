df645e14de02c27d51ec9644796de739b78bf840
Robert Collins <beaucollins@gmail.com>
2016-05-25 15:37:27 -0700

    Open chat from sidebar

--- client/layout/sidebar/footer.jsx
+++ client/layout/sidebar/footer.jsx
@@ -3,21 +3,34 @@
  */
 import React from 'react';
 import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
-var Gridicon = require( 'components/gridicon' );
-
+import Gridicon from 'components/gridicon';
 import Button from 'components/button';
+import { openChat } from 'state/live-chat/actions';
 
-const SidebarFooter = ( { translate, children } ) => (
+const SidebarFooter = ( { translate, children, onOpenChat } ) => (
 	<div className="sidebar__footer">
 		{ children }
-		<Button compact borderless href="/help">
+		<Button compact borderless href="/help" onClick={ onOpenChat }>
 			<Gridicon icon="help-outline" /> { translate( 'Help' ) }
 		</Button>
 	</div>
 );
 
-export default localize( SidebarFooter );
+const mapStateToProps = () => {
+	return {};
+};
+
+const mapDispatchToProps = ( dispatch ) => {
+	return {
+		onOpenChat() {
+			dispatch( openChat() );
+		}
+	};
+};
+
+export default connect( mapStateToProps, mapDispatchToProps )( localize( SidebarFooter ) );
